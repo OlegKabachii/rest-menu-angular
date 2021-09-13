9a0785976646a1048efa792fbc51b8ea9f93c290
OlegKabachii <kabachiyoleg@gmail.com>
2021-09-13 15:20:08 +0200

    first commit

---
+++ src/app/components/side-menu-page/side-menu-page.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SideMenuPageComponent } from './side-menu-page.component';
+
+describe('SideMenuPageComponent', () => {
+  let component: SideMenuPageComponent;
+  let fixture: ComponentFixture<SideMenuPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SideMenuPageComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SideMenuPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/side-menu-page/side-menu-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-side-menu-page',
+  templateUrl: './side-menu-page.component.html',
+  styleUrls: ['./side-menu-page.component.css']
+})
+export class SideMenuPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
